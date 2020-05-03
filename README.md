855a0c57539aae13d6d351bfe728d478142170f4
fkefer1 <friedrich.kefer@quoka.net>
2020-05-03 09:50:45 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # git-cli
+
+## Getting started
+
+### Project configuration
+ 
+ Start by cloning this project on your workstation.
+ 
+ ```bash
+ $ git clone git@github.com:https://github.com/kliefadas/git-cli.git git-cli
+ ```
+ 
+ The next thing will be to install all the dependencies of the project using [npm](https://www.npmjs.com/get-npm):
+ 
+ ```bash
+ $ cd ./git-cli
+ $ npm install
+ $ npm link
+ ```
+### Usage
+
+with "npm link" you made this cli globally available. Now you can navigate into your git projects and run `git-cli`
+
+ ```bash
+ $ cd ./some-git-project
+ $ git-cli
+ ```
+
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const simpleGit = require('simple-git/promise');
+const semver = require('semver');
+
+const git = simpleGit();
+
+async function init() {
+  const tags = await git.tags();
+  const latestTag = tags.latest
+  const semverChoices = [
+    'major',
+    'minor',
+    'patch'
+  ]
+
+  const generatedChoices = semverChoices.map(choice => choice + ' ' + semver.inc(latestTag, choice))
+  generatedChoices.push('prev ' + tags.all[tags.all.length - 2])
+  generatedChoices.push('other')
+
+  const {branchName} = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'branchName',
+      message: 'Which version you want to create?',
+      choices: ['release', 'hotfix']
+    }
+  ])
+
+  let version = semver.inc(latestTag, 'patch')
+  let baseBranch = 'master'
+
+  if (branchName === 'release') {
+    const {tagVersion} = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'tagVersion',
+        message: 'Which version you want to create?',
+        choices: generatedChoices
+      }
+    ]);
+    const [choice, selectedVersion] = tagVersion.split(' ')
+    version = selectedVersion
+
+    if (choice === 'other') {
+      const {customVersion} = await inquirer.prompt({
+        type: 'input',
+        name: 'customVersion',
+        message: 'Which custom version you want to create?',
+        validate(value) {
+          if (!semver.valid(value)) {
+            return 'version should be a valid semver value';
+          }
+          return true;
+        }
+      })
+      version = customVersion
+    }
+    else if (choice !== 'prev') {
+      const {selectedBaseBranch} = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'selectedBaseBranch',
+          message: 'Which base branch should release have?',
+          choices: ['develop', 'staging', 'master']
+        }
+      ])
+      baseBranch = selectedBaseBranch
+    }
+  }
+  // await git.push('origin', `origin/${baseBranch}:refs/heads/${branchName}/${version}`)
+
+  console.log(`Release ${branchName}/${version} got created based on ${baseBranch} branch`)
+}
+
+init()
+
